76b4fd488b742f60bc4b1c2b81d3cb441269dbc7
dylan <dylan204.play@gmail.com>
2022-03-23 17:06:10 -0500

    listo

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PaisSmall } from '../../interfaces/paises.interface';
 import { PaisesService } from '../../services/paises.service';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-selector-page',
@@ -10,15 +12,35 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPageComponent implements OnInit {
   form: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required],
   });
 
   //Llenar Regiones
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  frontera: PaisSmall[] = [];
 
   constructor(private fb: FormBuilder, private ps: PaisesService) {}
 
   ngOnInit(): void {
     this.regiones = this.ps.regiones;
+    //Cuando cambie la region
+    this.form
+      .get('region')
+      ?.valueChanges.pipe(
+        tap((_) => {
+          this.form.get('region')?.reset('');
+        }),
+        switchMap((region) => this.ps.getPaisesPorRegion(region))
+      )
+      .subscribe((paises) => {
+        this.paises = paises;
+      });
+
+    //Cuando cambie el pais
+    this.form.get('pais')?.valueChanges.subscribe((codigo) => {
+      console.log(codigo);
+    });
   }
 
   guardar() {
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +22,15 @@ export class PaisesService {
 
   constructor(private http: HttpClient) {}
 
-  getPaisesPorRegion(region: string) {
+  getPaisesPorRegion(region: string): Observable<Array<PaisSmall>> {
     const url: string = `${this.baseURL}/continent/${region}?fields=alpha3Code;name`;
-    return this.http.get(url);
+    return this.http.get<Array<PaisSmall>>(url);
+  }
+  getPaisesPorCodigo(codigo: string): Observable<Pais | null> {
+    if (!codigo) {
+      return of(null);
+    }
+    const url: string = `${this.baseURL}/alpha/${codigo}`;
+    return this.http.get<Pais>(url);
   }
 }
